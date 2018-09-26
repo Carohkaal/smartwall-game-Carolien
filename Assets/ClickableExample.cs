a3c34811796d4ccc09761b8b4f176f33d7262b13
Unknown <svenjanssen12321@gmail.com>
2018-09-26 09:34:17 +0200

    Added startup project + detection code

---
+++ Assets/ClickableExample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+                                //vvvvvvvvv laat je script van interactable inheriten!
+public class ClickableExample : Interactable {
+
+    public int ClickAmount;
+    public Text Label;
+
+
+	// Use this for initialization
+    protected override void Click()
+    {
+        ClickAmount++;
+        Label.text = "Aantal Clicks: " + ClickAmount;
+    }
+}
---
+++ Assets/Detection/Interactable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Interactable : MonoBehaviour
+{
+    bool _clicked;
+    float Cooldown = 30; //The amount of frames the interactable should not be hit again
+    // Prevents double inputs
+
+    public void Interact()
+    {
+        if (!_clicked)
+        {
+            StartCoroutine(_click());
+            Click();
+        }
+    }
+
+    protected virtual void Click()
+    {
+        throw new System.Exception("Click-Method not implemented!");
+    }
+
+    IEnumerator _click()
+    {   
+        _clicked = true;
+        float framecount = Cooldown;
+        while (framecount >= 0)
+        {
+            framecount--;
+            yield return null;
+        }
+
+        _clicked = false;
+    }
+}
